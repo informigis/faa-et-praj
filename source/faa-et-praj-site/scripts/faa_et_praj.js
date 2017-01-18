4a6cfb473660630c569163aac08a2d46d9848316
GISTEK\steenhr <steenhr@informi.dk>
2017-01-18 13:25:56 +0100

    address search added.

--- source/faa-et-praj-site/scripts/faa_et_praj.js
+++ source/faa-et-praj-site/scripts/faa_et_praj.js
@@ -1,15 +1,16 @@
 ﻿require([
-  "esri/Map",
-  "esri/views/MapView",
+  "esri/map",
+  "esri/dijit/Search",
   "dojo/domReady!"
-], function (Map, MapView) {
-    var map = new Map({
-        basemap: "streets"
-    });
-    var view = new MapView({
+], function (Map, Search) {
+    var map = new Map("viewDiv", {
+        basemap: "gray",
         container: "viewDiv",  // Reference to the scene div created in step 5
-        map: map,  // Reference to the map object created before the scene
         zoom: 12,  // Sets the zoom level based on level of detail (LOD)
         center: [9.47, 55.5]  // Sets the center point of view in lon/lat
     });
+    var search = new Search({
+        map: map
+    }, "search");
+    search.startup();
 });
