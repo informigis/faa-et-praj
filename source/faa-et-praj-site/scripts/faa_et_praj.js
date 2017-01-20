2e0b3b979ba7b2235f2e0b832a90bd20bb03c0bc
GISTEK\steenhr <steenhr@informi.dk>
2017-01-20 07:27:56 +0100

    now does something when clicked.

--- source/faa-et-praj-site/scripts/faa_et_praj.js
+++ source/faa-et-praj-site/scripts/faa_et_praj.js
@@ -22,9 +22,9 @@
         zoom: 12,  // Sets the zoom level based on level of detail (LOD)
         center: [9.47, 55.5]  // Sets the center point of view in lon/lat
     });
-    var wmsLayer = new WMSLayer('http://kortforsyningen.kms.dk/service?request=GetCapabilities&version=1.1.1&login=Kommune621&password=Qwertyu10&servicename=topo_skaermkort&service=WMS');
+    /*var wmsLayer = new WMSLayer('http://kortforsyningen.kms.dk/service?request=GetCapabilities&version=1.1.1&login=Kommune621&password=Qwertyu10&servicename=topo_skaermkort&service=WMS');
     var wmtsLayer = new WMTSLayer('http://kortforsyningen.kms.dk/?servicename=topo_skaermkort_daempet&client=arcGIS&request=GetCapabilities&service=WMTS&acceptversions=1.0.0&login=Kommune621&password=Qwertyu10', {
-    });
+    });*/
     var featureLayer = new FeatureLayer("http://gis.kolding.dk/arcgis/rest/services/PublicPlanByg/Lokalplaner/MapServer/0");
     //map.addLayer(wmsLayer);
     //map.addLayer(featureLayer);
@@ -45,6 +45,34 @@
 
     if (QueryString.email && QueryString.mobileNumber) {
         document.getElementById("deletePraj").style.display = "block";
-        updateElementValue("savePraj", "Gem ændringer");
+        updateElementValue("createPraj", "Gem ændringer");
     }
+
+    function deletePraj(e) {
+        console.log("Hello deletepraj!");
+        // get ids
+        // delete in database
+    }
+
+    function createPraj() {
+        console.log("Hello createPraj!");
+
+
+    }
+
+    function updatePraj() {
+
+    }
+
+    function addEventListener(element, eventHandlerFunction, eventType) {
+        var deletePrajElement = document.getElementById(element);
+        deletePrajElement.addEventListener(eventType, eventHandlerFunction, false);
+    }
+
+    addEventListener(deletePraj.name, deletePraj, "click");
+    addEventListener(createPraj.name, createPraj, "click");
+    //addEventListener(updatePraj.name, updatePraj, "click");
+
+    /*var deletePrajElement = document.getElementById("deletePraj");
+    deletePrajElement.addEventListener("click", deletePraj, false); */
 });
